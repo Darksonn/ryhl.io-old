018a6e580be517b53eff4316a809991cf2b92c78
Kristoffer Ryhl-Johansen <kristoffer@ryhl.dk>
2016-10-30 22:44:34 +0100

    remove unused javascript file

--- scripts/js-remove.js
+++
@@ -1,9 +0,0 @@
-
-function jsRemove() {
-  var jsremoves = document.getElementsByClassName("js-remove"), i;
-  for (i = 0; i < jsremoves.length; ++i) {
-    jsremoves[i].style = "display: none;";
-  }
-  if (prevload) prevload();
-};
-
